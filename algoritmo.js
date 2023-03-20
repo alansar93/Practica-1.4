4cacd14a53757f01659aa895416796dadbab0f4d
Alan <alansarchman@live.com.ar>
2023-03-20 19:50:50 -0300

    actividad 4

---
+++ algoritmo.js
@@ -0,0 +1,10 @@
+document.querySelector("body").innerHTML = `
+<summary>Esto es imporante</summary>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, expedita placeat odit ex incidunt voluptate
+            dolore a ullam asperiores doloremque voluptas amet obcaecati animi quis similique voluptates laborum
+            reprehenderit vero.</p>
+        <img height="450px" width="500px" src="https://definicion.de/wp-content/uploads/2013/03/perro-1.jpg" alt="">
+        <iframe height="450" width="500px" src="https://www.mercadolibre.com.ar/" frameborder="0"></iframe>
+       <br>
+        <a href="https://lapaginamillonaria.com/">LINK</a>
+    </details>`
